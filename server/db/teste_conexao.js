09a9f53a88bae8efc6ed65239b304369cf287e99
cleonildo nahum <cleonildo nahum>
2016-07-30 21:33:07 -0300

    Schemas Bike, station, user e loan adicionados

---
+++ server/db/teste_conexao.js
@@ -0,0 +1,11 @@
+var Mongoose = require('Mongoose');
+
+var db = Mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+  console.log('Conectado ao MongoDB.')
+  // Vamos adicionar nossos Esquemas, Modelos e consultas aqui
+});
+
+Mongoose.connect('mongodb://localhost/teste');
