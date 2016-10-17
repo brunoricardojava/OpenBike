3406f0db1bed2777f2a5b20e1ce9ae8b3e6c5997
Cleverson Nahum <cleversonahum@gmail.com>
2016-10-17 20:12:04 -0300

    Outra forma de adicionar o Banco de Dados

--- server/models/database.js
+++ server/models/database.js
@@ -10,19 +10,30 @@ var config = {
 };
 
 const client = new pg.Client(config);
+
+client.connect();
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
+
+
+/*var query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 //client.connect();
 client.connect(function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool
-    done();
+  // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+  //   //call `done()` to release the client back to the pool
+  //   done();
 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-    //output: 1
-  });
-});
+  //   if(err) {
+  //     return console.error('error running query', err);
+  //   }
+  //   console.log(result.rows[0].number);
+  //   //output: 1
+  // });
+  query.on('end', () => { client.end(); });
+
+});*/
