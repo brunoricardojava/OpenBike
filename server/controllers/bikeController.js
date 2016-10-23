41926eb873309cd9fc75a48361827d4360c18dff
Cleverson Nahum <cleversonahum@gmail.com>
2016-10-23 19:24:54 -0300

    api bike implemmented in according with issue #4

--- server/controllers/bikeController.js
+++ server/controllers/bikeController.js
@@ -9,12 +9,14 @@ var config = {
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
+const table_bike = bikes; //Name of table with bikes
+const table_loan = loan; //Name of table with bikes
 
 // Add Bike
 exports.addBike = function(req, res, next) {
   const results = [];
   // Grab data from http request
-  const data = {text: req.body.text, complete: false};
+  const data = {id_bike: req.body.id_bike, id_user: 0, status: "Works", cont: 0};
   // Get a Postgres client from the connection pool
   pg.connect(config, (err, client, done) => {
     // Handle connection errors
@@ -24,10 +26,10 @@ exports.addBike = function(req, res, next) {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Insert Data
-    client.query('INSERT INTO items(text, complete) values($1, $2)',
-    [data.text, data.complete]);
+    client.query('INSERT INTO ($1)(id_bike, id_user, status, cont) values($2, $3, $4, $5)',
+    [table_bike, data.id_bike, data.id_user, data.status, data.cont]);
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC');
+    const query = client.query('SELECT * FROM ($1) WHERE id_bike=($2) ORDER BY id ASC',[table_bike, data.id_bike]);
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
@@ -40,9 +42,11 @@ exports.addBike = function(req, res, next) {
   });
 }
 
-// Read Bike
-exports.readBike = function(req, res, next) {
+// Status Bike
+exports.statusBike = function(req, res, next) {
   const results = [];
+  const bike_id = req.params.bike_id; //Obtained through URL
+
   // Get a Postgres client from the connection pool
   pg.connect(config, (err, client, done) => {
     // Handle connection errors
@@ -52,7 +56,7 @@ exports.readBike = function(req, res, next) {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC;');
+    const query = client.query('SELECT * FROM ($1) WHERE id_bike=($2) ORDER BY id ASC;',[table_bike, bike_id]);
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
@@ -65,13 +69,11 @@ exports.readBike = function(req, res, next) {
   });
 }
 
-// Update bike
-exports.updateBike = function(req, res, next) {
+//Loan Bike
+exports.loanBike = function(req, res, next) {
   const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
   // Grab data from http request
-  const data = {text: req.body.text, complete: req.body.complete};
+  const data = {id_bike: req.body.id_bike, id_user: req.body.id_user, id_station: req.body.id_station, type: 'L'};
   // Get a Postgres client from the connection pool
   pg.connect(config, (err, client, done) => {
     // Handle connection errors
@@ -80,15 +82,25 @@ exports.updateBike = function(req, res, next) {
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    // SQL Query > Update Data
-    client.query('UPDATE items SET text=($1), complete=($2) WHERE id=($3)',
-    [data.text, data.complete, id]);
+
+    bike_cont = client.query("SELECT cont FROM ($1) WHERE id_bike=($2) ORDER BY id ASC",[table_bike, data.id_bike]);
+    bike_cont++; //Cont Quantity of Loans +1
+
+    // Update bike table
+    client.query('UPDATE ($1) SET id_user=($2), cont=($3) WHERE id_bike=($4)',
+    [table_bike, data.id_user, bike_cont, data.id_bike]);
+
+    // Add a registry for this loan in the table loan
+    client.query('INSERT INTO ($1)(id_bike, id_user, id_station, type) values($2, $3, $4, $5)',
+    [table_loan, data.id_bike, data.id_user, data.id_station, data.type]);
+
     // SQL Query > Select Data
-    const query = client.query("SELECT * FROM items ORDER BY id ASC");
+    const query = client.query("SELECT * FROM ($1) WHERE id_bike=($2) ORDER BY date DESC limit 1", [table_loan, data.id_bike]);
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
     });
+
     // After all data is returned, close connection and return results
     query.on('end', function() {
       done();
@@ -97,11 +109,11 @@ exports.updateBike = function(req, res, next) {
   });
 }
 
-//Delete Bike
-exports.deleteBike = function(req, res, next) {
+//Devolve Bike
+exports.devolveBike = function(req, res, next) {
   const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
+  // Grab data from http request
+  const data = {id_bike: req.body.id_bike, id_station: req.body.id_station, type: 'D' , id_user: req.body.id_user};
   // Get a Postgres client from the connection pool
   pg.connect(config, (err, client, done) => {
     // Handle connection errors
@@ -110,16 +122,62 @@ exports.deleteBike = function(req, res, next) {
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    // SQL Query > Delete Data
-    client.query('DELETE FROM items WHERE id=($1)', [id]);
+
+    // Update bike table
+    client.query('UPDATE ($1) SET id_user=0 WHERE id_bike=($2)',
+    [table_bike, data.id_bike]);
+
+    //var date = new Date(); //Date of system
+
+    // Update a registry for this loan in the table loan
+    client.query('UPDATE ($1) SET devolution=SYSDATETIME() WHERE id_bike=($2)',
+    [table_loan, data.id_bike]);
+
+
     // SQL Query > Select Data
-    var query = client.query('SELECT * FROM items ORDER BY id ASC');
+    const query = client.query("SELECT * FROM ($1) WHERE id_bike=($2) ORDER BY date DESC limit 1", [table_loan, data.id_bike]);
+
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
     });
+
     // After all data is returned, close connection and return results
-    query.on('end', () => {
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+}
+
+//Registry problem with a Bike
+exports.devolveBike = function(req, res, next) {
+  const results = [];
+  // Grab data from http request
+  const data = {id_bike: req.body.id_bike, status: req.body.status};
+  // Get a Postgres client from the connection pool
+  pg.connect(config, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+
+    // Update bike table
+    client.query('UPDATE ($1) SET status=($2) WHERE id_bike=($3)',
+    [table_bike, data.status, data.id_bike]);
+
+    // SQL Query > Select Data
+    const query = client.query("SELECT * FROM ($1) WHERE id_bike=($2) ORDER BY date DESC limit 1", [table_bike, data.id_bike]);
+
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
       done();
       return res.json(results);
     });
